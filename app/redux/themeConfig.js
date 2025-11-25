95a9b42ece513a8b6d33ba0fb86c2d5876b46bb2
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-11-25 09:05:09 +0530

    create themeConfig

---
+++ app/redux/themeConfig.js
@@ -0,0 +1,26 @@
+const darkTheme = {
+  background: "#0A1A2F",
+  secondaryBackground: "#000814",
+  text: "#FFFFFF",
+  secondaryText: "#79818D",
+  icon: "#18E7F2",
+  secondaryIcon: "#007BFF",
+  gradient: ["#0A1A2F", "#000814"],
+};
+
+const lightTheme = {
+  background: "#FFFFFF", 
+  secondaryBackground: "#F9FAFB", 
+  text: "#1F2937", 
+  secondaryText: "#6B7280", 
+  icon: "#FF9F40", 
+  secondaryIcon: "#3B82F6", 
+  gradient: ["#FFFFFF", "#F9FAFB"],
+};
+
+const themeConfig = {
+  dark: darkTheme,
+  light: lightTheme,
+};
+
+export default themeConfig;
